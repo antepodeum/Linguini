ac3ff9c3ea207b107a7ae7a8bb0887038489096a
Artemy Egorov <artegoser@gmail.com>
2026-05-24 14:34:55 +0300

    fix icons

--- site/src/routes/+layout.svelte
+++ site/src/routes/+layout.svelte
@@ -4,10 +4,4 @@
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" type="image/png" href="/icons/favicon-96x96.png?v=1" sizes="96x96" />
-	<link rel="icon" type="image/svg+xml" href="/icons/favicon.svg?v=1" />
-	<link rel="shortcut icon" href="/icons/favicon.ico?v=1" />
-</svelte:head>
-
 {@render children()}
